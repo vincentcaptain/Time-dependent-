c890f33f2b3c067db0e154afb84e2f15a3b9dc03
Shujia Liang <vincentcaptain@berkeley.edu>
2016-11-01 19:57:01 -0700

    updated

--- time_dependent_protocol.py
+++ time_dependent_protocol.py
@@ -62,12 +62,12 @@ def waiting_time(omega, total = 10, dt = 0.01, r0 = - 3, m = 1, gamma = 1, epsil
 # 	pp.close()
 # 	plt.show()
 
-omega = list(range(1, 30))
-o = [-i/4 for i in omega] + [i / 4 for i in omega]
+omega = list(range(1, 70))
+o = [-i/10 for i in omega] + [i / 10 for i in omega]
 omega = sorted(o)
 
 result = waiting_time(omega)
-np.savetxt("rate.txt", result)
+np.savetxt("rate_accurate.txt", result)
 # plotting(omega, result)
 
 
