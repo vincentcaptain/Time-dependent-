dd42b93212fc63aee994e584d0c9af8e70a1221b
Shujia Liang <vincentcaptain@berkeley.edu>
2016-11-08 10:41:31 -0800

    .

--- time_dependent_protocol.py
+++ time_dependent_protocol.py
@@ -1,7 +1,7 @@
 import numpy as np
-# import matplotlib.pyplot as plt
-# import matplotlib.mlab as mlab
-# from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from matplotlib.backends.backend_pdf import PdfPages
 from joblib import Parallel, delayed
 import multiprocessing
 
@@ -16,7 +16,7 @@ F_1D = lambda r, t, omega, epsilon: - r ** 3 + 9 * r - epsilon * np.sin(omega * 
 Xi = lambda gamma, beta, m, dt: np.random.normal(scale = np.sqrt(2 * m / beta * (1 - np.exp(- gamma * dt))))
 rescaling_c = lambda dt, gamma: np.sqrt(2 / gamma / dt * np.tanh(gamma * dt / 2))
 
-def waiting_time(i, total = 10, dt = 0.01, r0 = - 3, m = 1, gamma = 1, epsilon = 2, beta = 1):
+def waiting_time(i, total = 10, dt = 0.01, r0 = - 10, m = 1, gamma = 1, epsilon = 2, beta = 1):
 	"""
 	Parameters required for this simulation: time difference dt, initial position
 	r0, mass of particle m, damping constant gamma, time-dependent part Parameter
@@ -27,17 +27,15 @@ def waiting_time(i, total = 10, dt = 0.01, r0 = - 3, m = 1, gamma = 1, epsilon =
 	half dt, and position every dt; count the time it crosses the barrier until
 	10 times, finding the average time scale;
 	"""
-	t_continue, t_count, t_collection, crossing_collection, r_collection, left = 0, 0, [0], [], [r0], True
+	t_continue, t_collection, r_collection, left = 0, [0], [r0], True
 	p_half, p, c = 0, 0, rescaling_c(dt, gamma)
-	indicator_count, p_upper, p_lower, loop_count = 0, 0.05, -0.05, 1
-	probability = []
-	while len(crossing_collection) < total:
+	indicator_count, p_upper, p_lower, cross_count = 0, 0.05, -0.05, 0
+	while cross_count < total:
 		one_fourth_random = Xi(gamma, beta, m, dt)
 		three_fourth_random = Xi(gamma, beta, m, dt)
 		p_half = p * np.exp(- gamma * dt / 2) + one_fourth_random + F_1D(r0, t_continue, i, epsilon) * c * dt / 2
 		r0 = r0 + p_half * c * dt / m
 		t_continue += dt
-		t_count += dt
 		p = (p_half + F_1D(r0, t_continue, i, epsilon) * c * dt / 2) * np.exp(- gamma * dt / 2) + three_fourth_random
 		if r0 >= p_lower and r0 <= p_upper:
 			indicator_count += 1
@@ -45,12 +43,8 @@ def waiting_time(i, total = 10, dt = 0.01, r0 = - 3, m = 1, gamma = 1, epsilon =
 			left = not left
 		if (not left and r0 < 0):
 			left = not left
-			crossing_collection += [t_count]
-			probability += [indicator_count / t_count]
-			t_count = 0
-			indicator_count = 0
-	k = total / np.mean(crossing_collection) / 2
-	p_final = np.mean(probability)
+	k = total / t_continue
+	probability = indicator_count / t_continue
 	return k, p_final
 
 
