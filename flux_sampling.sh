154614b2d7be0f3a318fd691927a353b0266391b
Shujia Liang <vincentcaptain@berkeley.edu>
2016-11-13 12:11:46 -0800

    sh

--- flux_sampling.sh
+++ flux_sampling.sh
@@ -17,16 +17,4 @@
 # Mail user:
 #SBATCH --mail-user=vincentcaptain@berkeley.edu
 ## Command(s) to run:
-python2.7 -i time_dependent_protocol.py
-omega = list(range(1, 75))
-o = [-i/10 for i in omega] + [i / 10 for i in omega]
-omega = sorted(o)
-num_cores = multiprocessing.cpu_count()
-sample_size = 20
-interval = [-3, 3]
-steps = 60
-starting = -3.5
-final_p = Parallel(n_jobs = num_cores)(delayed(total_prob)(i, sample_size, interval, steps, starting) for i in omega)
-flux = Parallel(n_jobs = num_cores)(delayed(process_initial_flux)(i, sample_size, starting, interval[0]) for i in omega)
-np.savetxt("final_p.txt", final_p)
-np.savetxt("flux.txt", flux)
+python2.7 time_dependent_protocol.py
