03d850f44b8368f8ea48ac0754244ad03abdc6f7
Shujia Liang <vincentcaptain@berkeley.edu>
2016-11-13 16:32:00 -0800

    sh

--- time_dependent_protocol.py
+++ time_dependent_protocol.py
@@ -188,13 +188,18 @@ o = [-i/10 for i in omega] + [i / 10 for i in omega]
 omega = sorted(o)
 num_cores = multiprocessing.cpu_count()
 sample_size = 20
-interval = [-3, 3]
+interval = list(range(1, 61))
+inter = [-i/20 for i in interval] + [i / 20 for i in interval] + [0]
+interval = sorted(inter)
+l = list(range(0, 120))
 steps = 60
-starting = -3.5
-final_p = Parallel(n_jobs = num_cores)(delayed(total_prob)(i, sample_size, interval, steps, starting) for i in omega)
-flux = Parallel(n_jobs = num_cores)(delayed(process_initial_flux)(i, sample_size, starting, interval[0]) for i in omega)
+starting = -4
+init_p = process_initial_prob(2, sample_size, starting, interval[0])
+trans_p = Parallel(n_jobs = num_cores)(delayed(process_transitional_prob)(2, sample_size, interval[i], interval[i + 1]) for i in omega)
+final_p = init_p + trans_p
+# flux = Parallel(n_jobs = num_cores)(delayed(process_initial_flux)(i, sample_size, starting, interval[0]) for i in omega)
 np.savetxt("final_p.txt", final_p)
-np.savetxt("flux.txt", flux)
+# np.savetxt("flux.txt", flux)
 
 
 
