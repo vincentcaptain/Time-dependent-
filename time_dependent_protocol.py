1e508d33cca4755f454b87ed39797a824338493d
Shujia Liang <vincentcaptain@berkeley.edu>
2016-11-21 09:11:10 -0800

    log scale

--- time_dependent_protocol.py
+++ time_dependent_protocol.py
@@ -1,9 +1,10 @@
 import numpy as np
-# import matplotlib.pyplot as plt
-# import matplotlib.mlab as mlab
-# from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from matplotlib.backends.backend_pdf import PdfPages
 from joblib import Parallel, delayed
 import multiprocessing
+import random
 
 
 
@@ -17,7 +18,7 @@ F_1D = lambda r, t, omega, epsilon: - r ** 3 + 9 * r - epsilon * np.sin(omega * 
 Xi = lambda gamma, beta, m, dt: np.random.normal(scale = np.sqrt(m / beta * (1 - np.exp(- gamma * dt))))
 rescaling_c = lambda dt, gamma: np.sqrt(2 / gamma / dt * np.tanh(gamma * dt / 2))
 
-def waiting_time(i, total = 10, dt = 0.001, r0 = - 4, m = 1, gamma = 1, epsilon = 2, beta = 1):
+def waiting_time(i, total = 10, dt = 0.01, r0 = - 4, m = 1, gamma = 1, epsilon = 2, beta = 1):
 	"""
 	Parameters required for this simulation: time difference dt, initial position
 	r0, mass of particle m, damping constant gamma, time-dependent part Parameter
@@ -30,7 +31,7 @@ def waiting_time(i, total = 10, dt = 0.001, r0 = - 4, m = 1, gamma = 1, epsilon 
 	"""
 	t_continue, t_collection, left = 0, [0], True
 	p_half, c = 0, rescaling_c(dt, gamma)
-	p = np.random.normal(scale = max(1,np.sqrt(-V_rt(r0, 0, i, 2)/beta)))
+	p = np.random.normal(scale = np.sqrt(m/beta))
 	indicator_count, p_upper, p_lower, cross_count = 0, 0.05, -0.05, 0
 	while cross_count < total:
 		one_fourth_random = Xi(gamma, beta, m, dt)
@@ -51,13 +52,13 @@ def waiting_time(i, total = 10, dt = 0.001, r0 = - 4, m = 1, gamma = 1, epsilon 
 	probability = indicator_count / t_continue
 	return k, probability
 
-def initial_flux(i, From, To, limit = 0.5, dt = 0.001, m = 1, gamma = 1, epsilon = 2, beta = 1):
+def initial_flux(i, From, To, limit = 0.5, dt = 0.01, m = 1, gamma = 1, epsilon = 2, beta = 1):
 	"""
 	Initial flux is calculated by # of reach / t_obs, where # of reach should not be too far away.
 	The same position and velocity propagation and update methods as waiting_time.
 	"""
 	r0, t_obs = From, 0
-	p_half, p, c = 0, np.random.normal(scale = max(1,np.sqrt(-V_rt(From, 0, i, 2)/beta))), rescaling_c(dt, gamma)
+	p_half, p, c = 0, np.random.normal(scale = np.sqrt(m/beta)), rescaling_c(dt, gamma)
 	while r0 < To:
 		one_fourth_random = Xi(gamma, beta, m, dt)
 		three_fourth_random = Xi(gamma, beta, m, dt)
@@ -70,59 +71,62 @@ def initial_flux(i, From, To, limit = 0.5, dt = 0.001, m = 1, gamma = 1, epsilon
 	else:
 		return t_obs, True
 
-def initial_prob(i, From, To, dt = 0.001, m = 1, gamma = 1, epsilon = 2, beta = 1):
+def initial_prob(i, From, To, sample_size, dt = 0.01, m = 1, gamma = 1, epsilon = 2, beta = 1):
 	"""
 	Initial probability is calculated by # of reach / total # of attempts.
 	"""
-	r0, j, t_obs = From, 1, 0
+	r0, j, t_obs = From, 0, 0
 	p_half, c = 0, rescaling_c(dt, gamma)
-	p = np.random.normal(scale = max(1,np.sqrt(-V_rt(From, 0, i, 2)/beta)))
-	while r0 < To:
-		one_fourth_random = Xi(gamma, beta, m, dt)
-		three_fourth_random = Xi(gamma, beta, m, dt)
-		p_half = p * np.exp(- gamma * dt / 2) + one_fourth_random + F_1D(r0, t_obs, i, epsilon) * c * dt / 2
-		r0 = r0 + p_half * c * dt / m
-		t_obs += dt
-		p = (p_half + F_1D(r0, t_obs, i, epsilon) * c * dt / 2) * np.exp(- gamma * dt / 2) + three_fourth_random
-		j += 1
-	return 1 / j
+	p = np.random.normal(scale = np.sqrt(m/beta))
+	total, p_re, p_collection, t_collection = 0, [], [], []
+	while total < sample_size:
+		while r0 < To:
+			one_fourth_random = Xi(gamma, beta, m, dt)
+			three_fourth_random = Xi(gamma, beta, m, dt)
+			p_half = p * np.exp(- gamma * dt / 2) + one_fourth_random + F_1D(r0, t_obs, i, epsilon) * c * dt / 2
+			r0 = r0 + p_half * c * dt / m
+			t_obs += dt
+			p = (p_half + F_1D(r0, t_obs, i, epsilon) * c * dt / 2) * np.exp(- gamma * dt / 2) + three_fourth_random
+			j += dt
+		p_re += [1 / j]
+		p_collection += [p]
+		t_collection += [t_obs]
+		total += 1
+		r0, j, t_obs = From, 1, 0
+		p_half, c = 0, rescaling_c(dt, gamma)
+		p = np.random.normal(scale = np.sqrt(m/beta))
+	print(np.mean(p_re)/(1-np.mean(p_re)))
+	return np.mean(p_re), p_collection, t_collection
 
-def transition_prob(i, From, To, start, limit = 0.05, dt = 0.001, m = 1, gamma = 1, epsilon = 2, beta = 1):
-	forward, backward, r0, t_obs = 0, 0, From, 0
+def transition_prob(i, From, To, start, p_collection, t_collection, limit = 0.05, dt = 0.01, m = 1, gamma = 1, epsilon = 2, beta = 1):
+	forward, backward, r0, t_obs = 0, 0, From, random.choice(t_collection)
 	p_half, c = 0, rescaling_c(dt, gamma)
-	p = np.random.normal(scale = max(1,np.sqrt(-V_rt(From, 0, i, 2)/beta)))
-	r, rf, rb = 0, 0, 0
+	p = p_collection[t_collection.index(t_obs)]
+	p_collection1, t_collection1 = [], []
 	while forward < 20:
-		one_fourth_random = Xi(gamma, beta, m, dt)
-		three_fourth_random = Xi(gamma, beta, m, dt)
-		p_half = p * np.exp(- gamma * dt / 2) + one_fourth_random + F_1D(r0, t_obs, i, epsilon) * c * dt / 2
-		r0 = r0 + p_half * c * dt / m
-		t_obs += dt
-		p = (p_half + F_1D(r0, t_obs, i, epsilon) * c * dt / 2) * np.exp(- gamma * dt / 2) + three_fourth_random
+		while From <= r0 <= To:
+			one_fourth_random = Xi(gamma, beta, m, dt)
+			three_fourth_random = Xi(gamma, beta, m, dt)
+			p_half = p * np.exp(- gamma * dt / 2) + one_fourth_random + F_1D(r0, t_obs, i, epsilon) * c * dt / 2
+			r0 = r0 + p_half * c * dt / m
+			t_obs += dt
+			p = (p_half + F_1D(r0, t_obs, i, epsilon) * c * dt / 2) * np.exp(- gamma * dt / 2) + three_fourth_random
 		if r0 >= To:
 			forward += 1
-			r0 = From
-			t_obs = 0
-			p_half, p = 0, np.random.normal(scale = max(1,np.sqrt(-V_rt(From, 0, i, 2)/beta)))
-		if r0 <= start:
+			t_collection1 += [t_obs]
+			if p_half > p:
+				p_collection1 += [p_half]
+			else:
+				p_collection1 += [p]
+		else:
 			backward += 1
-			r0 = From
-			t_obs = 0
-			p_half, p = 0, np.random.normal(scale = max(1,np.sqrt(-V_rt(From, 0, i, 2)/beta)))
-	return forward / (forward + backward)
+		r0 = From
+		t_obs = random.choice(t_collection)
+		p_half, p = 0, p_collection[t_collection.index(t_obs)]
+	result = forward / (forward + backward)
+	return result, p_collection1, t_collection1
 
-def calc_interval(r, omega, t = 0, epsilon = 2):
-	inter = [i / 33 for i in r]
-	value = [V_rt(i, t, omega, epsilon) for i in inter]
-	yinterp = [-2*i for i in range(0, 11)]
-	x = np.array(inter)
-	y = np.array(value)
-	order = y.argsort()
-	y = y[order]
-	x = x[order]
-	interpret = lambda inp, x, y: np.interp(inp, y, x)
-	x_int = [interpret(i, x, y) for i in yinterp]
-	return sorted([-i for i in x_int] + [0] + [0.5, 1, 1.5, 2, 2.5])
+
 
 	
 
@@ -158,25 +162,19 @@ def process_initial_flux(omega, num_cross, From, To):
 			i += 1
 	return num_cross / t_obs 
 
-def process_initial_prob(omega, sample_size, From, To):
-	i, p_obs = 0, []
-	while i < sample_size:
-		p = initial_prob(omega, From, To)
-		p_obs += [p]
-		i += 1
-	return np.mean(p_obs) 
 
-
-def total_prob(omega, sample_size, starting):
+def total_prob(omega, sample_size, interval, starting):
 	"""
 	interval describes the range of calculation for transition_prob,
 	Steps describes number of transition_prob we calculated, and 
 	starting is the starting position of the particle.
 	"""
-	interval = calc_interval(list(range(1, 100)), omega)
 	From = starting
 	To = interval[0]
-	pi_zero = process_initial_prob(omega, sample_size, From, To)
+	pi = initial_prob(omega, From, To, sample_size)
+	pi_zero = pi[0]
+	p_collection = pi[1]
+	t_collection = pi[2]
 	final_p, p_track = pi_zero, [pi_zero]
 	i = 0
 	starting = interval[0]
@@ -184,22 +182,29 @@ def total_prob(omega, sample_size, starting):
 		From = To
 		i += 1
 		To = interval[i]
-		trans_p = transition_prob(omega, From, To, starting)
+		trans = transition_prob(omega, From, To, starting, p_collection, t_collection)
+		trans_p = trans[0]
+		print(trans_p)
+		p_collection = trans[1]
+		t_collection = trans[2]
 		final_p *= trans_p
-		p_track += [final_p]
-	return final_p
+		p_track += [trans_p]
+	return np.log(final_p)
 
 def accurate_k(omega, sample_size, interval, starting, final_p):
 	flux = process_initial_flux(omega, sample_size, starting, interval[0])
 	return flux * final_p
 		
-omega = list(range(1, 5))
-o = [-i for i in omega] + [i for i in omega]
+omega = list(range(1, 75))
+o = [-i/10 for i in omega] + [i/10 for i in omega]
 omega = sorted(o)
 num_cores = multiprocessing.cpu_count()
 sample_size = 20
-r = list(range(1, 100))
-starting = -4
+r = list(range(1, 13))
+starting = -3
+rl = [-i / 5 for i in r] + [i / 5 for i in r] + [0]
+r = sorted(rl)
+
 # result = Parallel(n_jobs = num_cores)(delayed(process_waiting_time)(i) for i in omega)
 # final_p = Parallel(n_jobs = num_cores)(delayed(total_prob)(i, sample_size, starting) for i in omega)
 # flux = Parallel(n_jobs = num_cores)(delayed(process_initial_flux)(i, sample_size, starting, calc_interval(r, i)[0]) for i in omega)
