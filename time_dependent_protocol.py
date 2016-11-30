fda474f68e1f79d6b10d1a3d3e5c143449d7d1ed
Shujia Liang <vincentcaptain@berkeley.edu>
2016-11-29 22:30:19 -0800

    sd

--- time_dependent_protocol.py
+++ time_dependent_protocol.py
@@ -1,5 +1,6 @@
-import numpy as np
 from __future__ import division
+import numpy as np
+
 # import matplotlib.pyplot as plt
 # import matplotlib.mlab as mlab
 # from matplotlib.backends.backend_pdf import PdfPages
